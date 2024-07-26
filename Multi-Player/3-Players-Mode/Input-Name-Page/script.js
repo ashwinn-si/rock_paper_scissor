efd5fc81a65c31103b5aa91ee5e0bcdf79a3bc63
ashwinn-si <ashwin23cse@gmail.com>
2024-07-26 21:54:34 +0530

    INPUT PAGE DONE..!

--- Multi-Player/3-Players-Mode/Input-Name-Page/script.js
+++ Multi-Player/3-Players-Mode/Input-Name-Page/script.js
@@ -3,40 +3,53 @@ document.getElementById("submit").addEventListener('click',()=>{
     main();
 });
 
+
 //function that checks whether the names are give as input
 
-function check_the_enter_name(p1,p2){
-    if(p1=='' && p2==''){
+function check_the_enter_name(p1,p2,p3){
+    if(p1=='' && p2=='' && p3==''){
         alert("NAMES NOT ENTERED");
         return(false);
-    }else if(p1==''){
+    }
+    else if((p1=='' && p2=='')||(p1=='' && p3=='')||(p2=='' || p3=='')){
+        alert("NAMES MISSING");
+        return(false)
+    }
+    else if(p1==''){
         alert("ENTER PLAYER 1 NAME");
         return(false);
     }else if(p2==''){
         alert("ENTER PLAYER 2 NAME");
         return(false);
-    }else{
+    }else if(p3==''){
+        alert("ENTER PLAYER 3 NAME");
+        return(false);
+    }
+    else{
         return(true);
     }
 }
-//functions that gets the player name;
+
+
+//functions that gets the player name
+
 function getting_player_name(){
     let player_1=document.getElementById('player_1_name').value;
     let player_2=document.getElementById('player_2_name').value;
-    if(check_the_enter_name(player_1,player_2)){
-        alert("hii");
-        return([player_1,player_2]);
+    let player_3=document.getElementById('player_3_name').value;
+    if(check_the_enter_name(player_1,player_2,player_3)){
+        return([player_1,player_2,player_3]);
         
     }
     
 }
-
 
 //function that stores the name in local storage
 
 function storing_names(names){
     localStorage.setItem("player_names",JSON.stringify(names));
 }
+
 
 function main(){
     let players_name=getting_player_name();
---
+++ Multi-Player/No-Player-Page/script.js
@@ -0,0 +1,8 @@
+//adding event listeners
+
+document.getElementById("player_2").addEventListener('click',()=>{
+    window.location.href="../2-Players-Mode/Input-Name-Page/index.html";
+});
+document.getElementById("player_3").addEventListener('click',()=>{
+    window.location.href="../3-Players-Mode/Input-Name-Page/index.html";
+});
