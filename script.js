911c134ac284fa3878c57bf766883a9391814a98
ashwinn-si <ashwin23cse@gmail.com>
2024-07-31 00:00:51 +0530

    Multi-Player-2-player-DONE..!

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ document.querySelector(".single_player_button").addEventListener('click',()=>{
     window.location.href="Single-player/Rounds/index.html";
 })
 document.querySelector(".multi_player_button").addEventListener('click',()=>{
-    alert("WILL BE CREATED SOON....!");
+    window.location.href="Multi-Player/No-Player-Page/index.html";
 })
